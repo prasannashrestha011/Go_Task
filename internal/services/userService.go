e61738f8a9618974d948d896138f660aaee3c9d3
prasannashrestha011 <shresthaprashanna27@gmail.com>
2025-12-16 21:02:10 +0545

    refactor: update UserService to use uuid.UUID for ID and enhance method signatures

--- internal/services/userService.go
+++ internal/services/userService.go
@@ -1,47 +1,109 @@
 package services
 
 import (
+	"log"
 	"main/internal/models"
 	"main/internal/repository"
+	"main/internal/utils"
+
+	"github.com/google/uuid"
 )
 
 type UserService interface {
-	RegisterUser(user *models.User) error
-	GetUserByID(id int64) (*models.User, error)
-	GetUserByEmail(email string) (*models.User, error)
-	UpdateUser(user *models.User) (*models.User, error)
-	DeleteUser(id int64) error
+	RegisterUser(user *models.UserCreateDTO) (*models.UserResponseDTO, error)
+	GetUserByID(id uuid.UUID) (*models.UserResponseDTO, error)
+	GetUsers() ([]*models.UserResponseDTO, error)
+	GetUserByEmail(email string) (*models.UserResponseDTO, error)
+	UpdateUser(id uuid.UUID, user *models.UserUpdateDTO) (*models.UserResponseDTO, error)
+	DeleteUser(id uuid.UUID) error
 }
 
 type userService struct {
 	repo repository.UserRepository
 }
 
-func NewUserRepository(repo repository.UserRepository) UserService {
+func (u *userService) GetUsers() ([]*models.UserResponseDTO, error) {
+	users,err:=u.repo.GetAll()
+	if err!=nil{
+		return nil,err
+	}
+
+	var usersDTO []*models.UserResponseDTO
+	for _,user:=range users{
+		dto:=&models.UserResponseDTO{
+			ID:user.ID ,
+			Name: user.Name,
+			Email: user.Email,
+			CreatedAt: user.CreatedAt,
+			UpdatedAt: user.UpdatedAt,
+		}
+		usersDTO = append(usersDTO,dto )
+	}
+	return usersDTO,nil
+}
+
+func NewUserService(repo repository.UserRepository) UserService {
 	return &userService{repo: repo}
 }
-// DeleteUser implements [UserService].
-func (u *userService) DeleteUser(id int64) error {
-	panic("unimplemented")
+
+func (u *userService) RegisterUser(dto *models.UserCreateDTO) (*models.UserResponseDTO, error) {
+	user := &models.User{
+		Name:     dto.Name,
+		Email:    dto.Email,
+		Password: dto.Password,
+	}
+	err := u.repo.Create(user)
+	if err != nil {
+		return nil, err
+	}
+	userDTO := utils.ToUserResponseDTO(user)
+	return userDTO, nil
 }
 
-// GetUserByEmail implements [UserService].
-func (u *userService) GetUserByEmail(email string) (*models.User, error) {
-	panic("unimplemented")
+func (u *userService) DeleteUser(id uuid.UUID) error {
+	return u.repo.Delete(id)
 }
 
-// GetUserByID implements [UserService].
-func (u *userService) GetUserByID(id int64) (*models.User, error) {
-	panic("unimplemented")
+func (u *userService) UpdateUser(id uuid.UUID, dto *models.UserUpdateDTO) (*models.UserResponseDTO, error) {
+
+	user, err := u.repo.GetByID(id)
+	if err != nil {
+		return nil, err
+	}
+	log.Println(dto)
+	if dto.Name != "" {
+		user.Name = dto.Name
+	}
+
+	if dto.Email != "" {
+		user.Email = dto.Email
+	}
+	if dto.Password != "" {
+		user.Password = dto.Password
+	}
+
+	updated_user, err := u.repo.Update(user)
+	if err != nil {
+		return nil, err
+	}
+	userDTO := utils.ToUserResponseDTO(updated_user)
+	return userDTO, nil
 }
 
-// RegisterUser implements [UserService].
-func (u *userService) RegisterUser(user *models.User) error {
-	panic("unimplemented")
+func (u *userService) GetUserByEmail(email string) (*models.UserResponseDTO, error) {
+	user, err := u.repo.GetByEmail(email)
+	if err != nil {
+		return nil, err
+	}
+	userDTO := utils.ToUserResponseDTO(user)
+	return userDTO, nil
 }
 
-// UpdateUser implements [UserService].
-func (u *userService) UpdateUser(user *models.User) (*models.User, error) {
-	panic("unimplemented")
+func (u *userService) GetUserByID(id uuid.UUID) (*models.UserResponseDTO, error) {
+	user, err := u.repo.GetByID(id)
+	if err != nil {
+		return nil, err
+	}
+	userDTO := utils.ToUserResponseDTO(user)
+	return userDTO, nil
 }
-
