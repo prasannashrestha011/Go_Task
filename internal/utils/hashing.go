98d937121e91b623a0e8c90da813d77bfc39af63
prasannashrestha011 <shresthaprashanna27@gmail.com>
2025-12-16 21:19:03 +0545

    add: defined hash methods

--- internal/utils/hashing.go
+++ internal/utils/hashing.go
@@ -1,1 +1,16 @@
 package utils
+
+import "golang.org/x/crypto/bcrypt"
+
+func HashPassoword(password string) (string, error) {
+	hashed, err := bcrypt.GenerateFromPassword([]byte(password),bcrypt.DefaultCost)
+	if err!=nil{
+		return "",err
+	}
+	return string(hashed),nil
+}
+
+func ComparePassword(hashed string,plain string)bool{
+	err:=bcrypt.CompareHashAndPassword([]byte(hashed),[]byte(plain))
+	return err==nil
+}
