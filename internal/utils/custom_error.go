6309835782d36776184a31cbc0a831823485add5
prasannashrestha011 <shresthaprashanna27@gmail.com>
2025-12-17 21:36:24 +0545

    update: minor changes in app error fields

--- internal/utils/custom_error.go
+++ internal/utils/custom_error.go
@@ -3,11 +3,26 @@ package utils
 //custom error type
 // status->true(success)
 // status->false(failed)
-type CustomError struct {
-	Status  bool
+type AppError struct {
 	Message string
+	Err error
+	Code int
 }
 
-func (e *CustomError) Error() string{
+func (e *AppError) Error() string{
 	return e.Message
 }
+
+func New(code int,message string,err error) *AppError{
+
+	return &AppError{
+		Message: message,
+		Code: code,
+		Err:err,
+		}
+}
+
+var (
+	ErrBadRequest=New(400,"Invalid request body",nil)
+	ErrUnAuthorized=New(401,"Request unauthorized",nil)
+)
