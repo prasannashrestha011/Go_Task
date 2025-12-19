ee55d874aba42f85a64835398050c26dc1b02d20
prasannashrestha011 <shresthaprashanna27@gmail.com>
2025-12-19 22:04:13 +0545

    feat: added dedicated schema for response and error for better clarity in the response

--- internal/schema/responseSchema.go
+++ internal/schema/responseSchema.go
@@ -1,6 +1,34 @@
 package schema
 
-type ResponseSchema struct {
-	Message string
-	Code    int
+type Response struct {
+	Success bool         `json:"success"`
+	Message string       `json:"message,omitempty"`
+	Data    interface{}  `json:"data,omitempty"`
+	Error   *ErrorDetail `json:"error,omitempty"`
+}
+
+// Error Body is explicity used instead of App error to prevent sending internal app errors to the client side.
+type ErrorDetail struct {
+	Code    string `json:"code"`
+	Message string `json:"message"`
+	Details string `json:"details,omitempty"`
+}
+
+func SuccessResponse(data interface{}, message string) Response {
+	return Response{
+		Success: true,
+		Message: message,
+		Data:    data,
+	}
+}
+
+func ErrorResponse(code, message, details string) Response {
+	return Response{
+		Success: false,
+		Error: &ErrorDetail{
+			Code:    code,
+			Message: message,
+			Details: details,
+		},
+	}
 }
