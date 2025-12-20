b89c767ac427d8a2aca04d1b82349e62717ee46b
prasannashrestha011 <shresthaprashanna27@gmail.com>
2025-12-20 11:23:13 +0545

    add: order update schema

--- internal/schema/orderSchema.go
+++ internal/schema/orderSchema.go
@@ -11,16 +11,23 @@ type CreateOrder struct{
 	OrderName string `json:"orderName" binding:"required"`
 	Amount float64 `json:"amount" binding:"required"`
 }
+type OrderUpdate struct{
+	OrderName *string `json:"order_name"`
+	Amount *float64 `json:"amount"`
+	Status *string `json:"status"`
+}
 type OrderResponse struct {
 	ID uuid.UUID `json:"id"`
 	UserID uuid.UUID `json:"userID"`
+	OrderName string `json:"order_name"`
 	Amount float64 `json:"amount"`
-	CreatedAT time.Time `json:"createdAt"`
+	CreatedAt time.Time `json:"createdAt"`
 	Status string `json:"status"`
 }
 
 type UserOrderResponse struct{
 	ID uuid.UUID `json:"id"`
+	OrderName string `json:"order_name"`
 	Amount float64 `json:"amount"`
 	CreatedAt time.Time `json:"createdAt"`
 	Status string `json:"status"`
