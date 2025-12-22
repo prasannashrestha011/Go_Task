57094f40c45efccbbfa78ed5440ed326e598f056
prasannashrestha011 <shresthaprashanna27@gmail.com>
2025-12-22 19:07:44 +0545

    added: goroutine to clean up inactive IPs

--- internal/utils/rate_limiter.go
+++ internal/utils/rate_limiter.go
@@ -1,9 +1,11 @@
 package utils
 
 import (
+	"main/internal/logger"
 	"sync"
 	"time"
 
+	"go.uber.org/zap"
 	"golang.org/x/time/rate"
 )
 
@@ -48,4 +50,23 @@ func AllowRequest(ip string, limit rate.Limit, burst int) (bool,time.Duration){
 		return false,delay
 	}
 	return true,0 
+}
+
+
+// goroutine to clean up rate limited client IPs that have been in active for specified amount of time
+func CleanUpLimits(lastSeenThrestHold time.Duration){
+
+	ticker:=time.NewTicker(time.Minute)
+	defer ticker.Stop()
+	for range ticker.C{
+		Mu.Lock()
+		now:=time.Now()
+		for ip,client:=range Clients{
+			if now.Sub(client.lastSeen)>lastSeenThrestHold{
+				logger.Log.Info("Deleteing IP",zap.String("IP",ip))
+				delete(Clients,ip)
+			}
+		}
+		Mu.Unlock()
+	}
 }
